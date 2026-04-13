6a256abd1b1355bcb3eb34c451572a7e1a396e8e
Ole André Vadla Ravnås <oleavr@frida.re>
2026-04-13 20:17:32 +0200

    Use remote SwiftLibXML on all platforms
    SwiftLibXML no longer uses unsafeFlags on Windows, so
    it can be referenced as a remote dependency everywhere.
    Remove the platform-conditional local path workaround.

--- Package.swift
+++ Package.swift
@@ -6,24 +6,11 @@ let pkgName = "gir2swift"
 let libTarget = "lib\(pkgName)"
 let plugin = "\(pkgName)-plugin"
 
-// On Windows, SPM refuses to build packages that use `unsafeFlags` when fetched
-// as a remote dependency.  SwiftLibXML requires unsafeFlags on Windows (because
-// SPM's systemLibrary/pkgConfig mechanism does not interoperate with MSYS2's
-// pkgconf), so it must be referenced as a local (path-based) dependency there.
-// On macOS and Linux the normal remote URL is used.
-#if os(Windows)
 var packageDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.6"),
-    .package(path: "../SwiftLibXML"),
+    .package(url: "https://github.com/frida/SwiftLibXML.git", branch: "main"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.1"),
 ]
-#else
-var packageDependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.6"),
-    .package(url: "https://github.com/rhx/SwiftLibXML.git", branch: "main"),
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.1"),
-]
-#endif
 
 #if compiler(>=6.2)
 packageDependencies.append(
@@ -41,7 +28,7 @@ let package = Package(
     dependencies: packageDependencies,
     targets: [
         .executableTarget(
-            name: pkgName, 
+            name: pkgName,
             dependencies: [
                 .init(stringLiteral: libTarget)
             ]
